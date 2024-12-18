473cc888fc543cf4a995e85f524f2d81427a89c3
Toxaencom1 <Toxaencom@gmail.com>
2024-12-18 23:11:12 +0500

    added migration scripts for insert default countries and cities, also added 01-initial-schema.xml

---
+++ src/main/resources/db/changelog/sql/add_defaults_countries_and_cities.sql
@@ -0,0 +1,7 @@
+insert into JavaRush.country (id, name)
+values (1,'Kazakhstan'),
+       (2,'Russia');
+insert into JavaRush.city (id, country_id, name)
+values (1,1,'Almaty'),
+       (2,1,'Astana'),
+        (3,2,'Novosibirsk');
