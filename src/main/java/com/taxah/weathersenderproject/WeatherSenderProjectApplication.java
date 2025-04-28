403f3f78b31bed3d171d96e30c9a15ccec21d397
Toxaencom1 <Toxaencom@gmail.com>
2025-04-28 11:34:35 +0500

    Version 0.0.1 commit. Added app version

--- src/main/java/com/taxah/weathersenderproject/WeatherSenderProjectApplication.java
+++ src/main/java/com/taxah/weathersenderproject/WeatherSenderProjectApplication.java
@@ -2,6 +2,7 @@ package com.taxah.weathersenderproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,7 +10,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class WeatherSenderProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeatherSenderProjectApplication.class, args);
+        SpringApplication app = new SpringApplication(WeatherSenderProjectApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+
+        System.out.println("===============================================");
+        System.out.println("          Application version: " + env.getProperty("spring.application.version"));
+        System.out.println("===============================================");
     }
 
 }
